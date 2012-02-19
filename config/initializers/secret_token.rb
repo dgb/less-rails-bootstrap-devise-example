f76e416c3fd667bc3776d5d3640c6b2eb3877023
Derek Barnes <derek@bigbento.com>
2012-02-18 16:26:08 -0800

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LessRailsBootstrapDeviseExample::Application.config.secret_token = '49085b7e8532f0c9e5e2fd479fd14cd5be35c8b928e855cd13aba882b908c0e7c456ac1ba735a792a30ef765962ef01317c7e8ad8556b1b346a1c4d7f73f4813'
